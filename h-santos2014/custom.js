87f823560e6cf01be329d3d63586749a73fb0ece
Iñigo Medina <imedina@grosshat.com>
2014-10-14 12:40:45 +0200

    Add markup skeleton with content for Hotel Madrid Praga.

--- h-santos2014/custom.js
+++ h-santos2014/custom.js
@@ -1,15 +1,20 @@
 /*
  * Smooth scrolling for nav sections.
  */
-Mirai.query('.menu-anchor, .header-photos').click(function(evt){
+Mirai.query('.menu-anchor, .header-photos, .header-reviews a').click(function(evt){
   evt.preventDefault();
   var link = Mirai.query(this).attr('href');
   var pos = Mirai.query(link).offset().top;
   Mirai.query('html,body').animate({scrollTop:pos-50},700);
-});	
+}); 
 
-
-Mirai.query('#ngg-gallery-4-19_container').galleryView({
+/*
+ * Custom options for film-gallery.
+ */
+Mirai.query('#gallery-film').galleryView({
+    panel_animation: 'crossfade',
+    transition_speed: 2000,
+    transition_interval: 5000,
     frame_width: 100,
     frame_height: 75,
     frame_gap: 0,
